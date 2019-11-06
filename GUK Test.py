b95b786c1f511c9618a2857f429ec822d022ad26
Rostislav <38621717+Bogaevskiy@users.noreply.github.com>
2019-11-06 20:21:39 +0300

    Add files via upload

--- GUK Test.py
+++ GUK Test.py
@@ -3,5 +3,7 @@ def file_search(file, key):
     f = open(file, 'r')
     re.split('\x0A|\t', f)
     dict = {f[i]: f[i+1] for i in range(0, len(f), 2)}
-    return dict[key]
-    
+    try:
+        return dict[key]
+    except:
+        return 'undef'
